59204bcc9d741e10a3a8a7170f0abb6cb7a2f630
Khloe Ramdhan <75541476+khloe-r@users.noreply.github.com>
2021-06-24 03:42:23 +0000

    implemented user input

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import json
 import os
 TOKEN = os.environ['FINNHUB_API_KEY'] #Secret Environment Variable
 
+# Asks user for Stock to search
+line = input("Give me a stock symbol? ")
+
+# Makes request to the API
 header = {'X-Finnhub-Token': TOKEN}
-r = requests.get(f'https://finnhub.io/api/v1/quote?symbol=AAPL', headers = header)
+r = requests.get(f'https://finnhub.io/api/v1/quote?symbol={line}', headers = header)
 
 #print(r.content)
 # c - current price
@@ -16,4 +20,16 @@ r = requests.get(f'https://finnhub.io/api/v1/quote?symbol=AAPL', headers = heade
 # pc - close price (previous day)
 # t - timestamp
 
-dictionary = json.loads(r.content)
+# Converts Response data type to a python dictionary
+pydict = json.loads(r.content)
+
+# Calculate percent change since day prior
+value = pydict['c']
+previousClose = pydict['pc']
+percentChange = ((value - previousClose)/previousClose) * 100
+
+# Output desired information
+print(line + ':')
+print("\t Current Value", "$" + str(value), sep = " - ")
+print("\t Previous Close", "$" + str(previousClose), sep = " - ")
+print("\t Percent Change", "{:.3f}".format(percentChange)+"%", sep = " - ")
